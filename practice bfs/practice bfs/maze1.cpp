4b6ddb5e17e0339c61b631dea3405b56aeee0e0e
inguuu <dlsrb8949@gmail.com>
2019-08-26 16:11:09 +0900

    commit maze1#6

--- practice bfs/practice bfs/maze1.cpp
+++ practice bfs/practice bfs/maze1.cpp
@@ -5,23 +5,35 @@ using namespace std;
 
 int main(){
 
+	queue <pair <pair <int, int>, int>> q;
 	int count = 0;
 	int N,M;
 	int S_r, S_c;
 	int E_r, E_c;
 
-	int map[1000][1000];
+	int map[1001][1001];
 	
 	cin >> count;
-	cin >> N;
-	cin >> M;
-    
-	cin >> S_r;
-	cin >> S_c;
-	cin >> E_r;
-	cin >> E_c;
+	
 
-	for (int i = 0; i < count; i++) {
+	for (int t = 0; t < count; t++) {
+
+		for (int i = 0; i <= N; i++) {
+			for (int j = 0; j <= M; j++) {
+				map[i][j] = 0;
+			}
+		}
+		while (!q.empty()) {
+			q.pop();
+		}
+		cin >> N;
+		cin >> M;
+
+		cin >> S_r;
+		cin >> S_c;
+		cin >> E_r;
+		cin >> E_c;
+
 
 
 	}
