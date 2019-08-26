f7966ba075068c66252a57832f4c3b5abecab13f
inguuu <dlsrb8949@gmail.com>
2019-08-26 18:58:41 +0900

    maze#2

--- practice bfs/practice bfs/maze2(2178).cpp
+++ practice bfs/practice bfs/maze2(2178).cpp
@@ -12,15 +12,57 @@ int main(){
 	//µ¿³²¼­ºÏ
 	int dr[] = { 0,1,0,-1 };
 	int dc[] = { 1,0,-1,0 };
+	int S_r, S_c;
+	int E_r, E_c;
 
 	cin >> N;
 	cin >> M;
 
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < M; j++) {
+	
+	for (int i = 1; i < N+1; i++) {
+		for (int j = 1; j < M+1; j++) {
 			cin >> map[i][j];
 		}
 	}
+
+	S_r = 1;
+	S_c = 1;
+	q.push(make_pair(make_pair(S_r, S_c), 0));
+
+	int answer = 0;
+
+	/*while (!q.empty()) {
+
+		int now_row = q.front().first.first;
+		int now_col = q.front().first.second;
+		int now_count = q.front().second;
+		q.pop();
+		map[now_row][now_col] = 1;
+
+		if (now_row == N && now_col == M) {
+			answer = now_count;
+			break;
+		}
+		else {
+			for (int i = 0; i < 4; i++) {
+
+				int nxt_row = now_row + dr[i];
+				int nxt_col = now_col + dc[i];
+				int nxt_count = ++now_count;
+				
+				if (nxt_row>=2&&nxt_row<=N&&nxt_col>=2&&nxt_col<=M){
+					if (map[nxt_row][nxt_col] == 0) {
+						q.push(make_pair(make_pair(nxt_row, nxt_col), nxt_count));
+
+					}
+				}
+
+			}
+		}
+
+	}*/
+
+	cout << answer << endl;
 }
 
 /*
