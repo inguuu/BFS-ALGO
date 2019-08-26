1329b5fe910f0da7f4c8e932bccde49236e6823c
inguuu <dlsrb8949@gmail.com>
2019-08-26 18:45:36 +0900

    maze#1

--- practice bfs/practice bfs/maze1.cpp
+++ practice bfs/practice bfs/maze1.cpp
@@ -5,119 +5,119 @@
 #include<queue>
 using namespace std;
 
-int main(){
-
-	
-	
-	queue <pair <pair <int, int>, int>> q; //시작점 이동횟수 삽입
-	int count = 0;
-	int N, M;
-	int S_r, S_c;
-	int E_r, E_c;
-
-	//동남서북 나중에 포문으로 사용된다 하나의 쌍으로 
-	int dr[] = { 0,1,0,-1 };
-	int dc[] = { 1,0,-1,0 };
-
-	int map[100][100];
-
-	int answer;
-	
-	cin >> count;
-	
-	
-	for (int t = 0; t < count; t++) {
-
-		// 입력 받기 
-		cin >> N;
-		cin >> M;
-
-		// 배열 초기화 및 큐 초기화
-		for (int i = 0; i <= N+1; i++) {
-			for (int j = 0; j <= M+1; j++) {
-				map[i][j] = 0;
-			}
-		}
-		while (!q.empty()) {
-			q.pop();
-		}
-
-		cin >> S_r;
-		cin >> S_c;
-		cin >> E_r;
-		cin >> E_c;
-
-
-		//시작하기 
-
-		q.push(make_pair(make_pair(S_r, S_c), 0));
-		map[S_r][S_c] = 1;
-
-
-		for (int i = 1; i <= N; i++) {
-			for (int j = 1; j < M; j++) {
-				cin >> map[i][j];
-			}
-		}
-
-		// go
-		answer = -1;
-		int ig = 0;
-		while (!q.empty()) {
-			
-			int now_row = q.front().first.first;
-			int now_col = q.front().first.second;
-			int now_count = q.front().second;
-		/*	cout << "=====ig=====" << endl;
-			cout << ig <<endl;
-			++ig;
-			cout << "=====now=====" << endl;
-			cout << now_row<<","<<now_col << endl;*/
-			
-			q.pop();
-
-			//정답
-			if (now_row == E_r && now_col == E_c) {
-				answer = now_count;
-				break;
-			}
-		
-
-			//과정
-			else {
-				for (int i = 0; i < 4; i++) {
-					int nxt_row = now_row + dr[i];
-					int nxt_col = now_col + dc[i];
-					int nxt_count = now_col+1;
-
-				/*	cout << "=====now for=====" << endl;
-					cout << "i: " << i << endl;
-					cout << nxt_row << "," << nxt_col << endl;
-					cout << map[nxt_row][nxt_col] << endl;*/
-					if (nxt_row >= 1 && nxt_row <= N && nxt_col >= 1 && nxt_col <= M) {
-						if (map[nxt_row][nxt_col] != 1) {
-
-						/*	cout << "=====next====="<<endl;
-							cout << "i: "<<i << endl;
-							cout << nxt_row << "," << nxt_col << endl;*/
-							map[nxt_row][nxt_col] = 1;
-							q.push(make_pair(make_pair(nxt_row, nxt_col), nxt_count));
-					   }
-					}
-					
-				}
-			}
-
-		}
-	
-		cout << "#" << t + 1 << " " << answer;
-
-
-	}
-	
-	return 0;
-
-}
+//int main(){
+//
+//	
+//	
+//	queue <pair <pair <int, int>, int>> q; //시작점 이동횟수 삽입
+//	int count = 0;
+//	int N, M;
+//	int S_r, S_c;
+//	int E_r, E_c;
+//
+//	//동남서북 나중에 포문으로 사용된다 하나의 쌍으로 
+//	int dr[] = { 0,1,0,-1 };
+//	int dc[] = { 1,0,-1,0 };
+//
+//	int map[100][100];
+//
+//	int answer;
+//	
+//	cin >> count;
+//	
+//	
+//	for (int t = 0; t < count; t++) {
+//
+//		// 입력 받기 
+//		cin >> N;
+//		cin >> M;
+//
+//		// 배열 초기화 및 큐 초기화
+//		for (int i = 0; i <= N+1; i++) {
+//			for (int j = 0; j <= M+1; j++) {
+//				map[i][j] = 0;
+//			}
+//		}
+//		while (!q.empty()) {
+//			q.pop();
+//		}
+//
+//		cin >> S_r;
+//		cin >> S_c;
+//		cin >> E_r;
+//		cin >> E_c;
+//
+//
+//		//시작하기 
+//
+//		q.push(make_pair(make_pair(S_r, S_c), 0));
+//		map[S_r][S_c] = 1;
+//
+//
+//		for (int i = 1; i <= N; i++) {
+//			for (int j = 1; j < M; j++) {
+//				cin >> map[i][j];
+//			}
+//		}
+//
+//		// go
+//		answer = -1;
+//		int ig = 0;
+//		while (!q.empty()) {
+//			
+//			int now_row = q.front().first.first;
+//			int now_col = q.front().first.second;
+//			int now_count = q.front().second;
+//		/*	cout << "=====ig=====" << endl;
+//			cout << ig <<endl;
+//			++ig;
+//			cout << "=====now=====" << endl;
+//			cout << now_row<<","<<now_col << endl;*/
+//			
+//			q.pop();
+//
+//			//정답
+//			if (now_row == E_r && now_col == E_c) {
+//				answer = now_count;
+//				break;
+//			}
+//		
+//
+//			//과정
+//			else {
+//				for (int i = 0; i < 4; i++) {
+//					int nxt_row = now_row + dr[i];
+//					int nxt_col = now_col + dc[i];
+//					int nxt_count = now_col+1;
+//
+//				/*	cout << "=====now for=====" << endl;
+//					cout << "i: " << i << endl;
+//					cout << nxt_row << "," << nxt_col << endl;
+//					cout << map[nxt_row][nxt_col] << endl;*/
+//					if (nxt_row >= 1 && nxt_row <= N && nxt_col >= 1 && nxt_col <= M) {
+//						if (map[nxt_row][nxt_col] != 1) {
+//
+//						/*	cout << "=====next====="<<endl;
+//							cout << "i: "<<i << endl;
+//							cout << nxt_row << "," << nxt_col << endl;*/
+//							map[nxt_row][nxt_col] = 1;
+//							q.push(make_pair(make_pair(nxt_row, nxt_col), nxt_count));
+//					   }
+//					}
+//					
+//				}
+//			}
+//
+//		}
+//	
+//		cout << "#" << t + 1 << " " << answer;
+//
+//
+//	}
+//	
+//	return 0;
+//
+//}
 
 /*
 
