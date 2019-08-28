bc4fe3bbae66cebac9994af12e360092bafa9774
inguuu <dlsrb8949@gmail.com>
2019-08-28 14:22:42 +0900

    &&보다 ||으로 범위를 두고 continue를 쓰자

--- practice bfs/practice bfs/mytomato.cpp
+++ practice bfs/practice bfs/mytomato.cpp
@@ -5,29 +5,29 @@
 #include<queue>
 using namespace std;
 
-int main(){
+int main() {
 
 	int n, m;
 	queue < pair<int, int >> q;
-     
+
 	int map[1002][1002];
 	int visited[1002][1002];
-	
-	int dr[] = {1,0,-1,0};
-	int dc[] = {0,1,0,-1};
-	
+
+	int dr[] = { 1,0,-1,0 };
+	int dc[] = { 0,1,0,-1 };
+
 	cin >> m;
 	cin >> n;
 
-	for (int i = 0; i <n; i++) {
+	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < m; j++) {
 			cin >> map[i][j];
 			visited[i][j] = 0;
 			if (map[i][j] == 1) {
 				q.push(make_pair(i, j));
 				visited[i][j] = 1;
-				
-			}	
+
+			}
 		}
 	}
 
@@ -37,41 +37,42 @@ int main(){
 		while (size--) {
 			int now_row = q.front().first;
 			int now_col = q.front().second;
+			//visited[now_row][now_col] = 1;
 			q.pop();
 
 			for (int i = 0; i < 4; i++) {
 				int nxt_row = now_row + dr[i];
 				int nxt_col = now_col + dc[i];
 				if (nxt_row < 0 || nxt_col < 0 || nxt_row >= n || nxt_col >= m) continue;
-				if (visited[nxt_row][nxt_col] == 0 && map[nxt_row][nxt_col] == 0) {
+					if (visited[nxt_row][nxt_col] == 0 && map[nxt_row][nxt_col] == 0) {
+						//cout <<nxt_row<<", "<<nxt_col << " "<< map[nxt_row][nxt_col] <<endl;
 						visited[nxt_row][nxt_col] = 1;
 						map[nxt_row][nxt_col] = map[now_row][now_col] + 1;
 						q.push(make_pair(nxt_row, nxt_col));
-					
-				}
+
+					}
+				
 			}
+
 		}
-		if (q.empty())
+		if (q.empty()) 
 			break;
 
 		now_count++;
 	}
 
-	
-	
-	
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < m; j++) {
 			if (map[i][j] == 0) {
-			
+
 				cout << "-1" << endl;
 				return 0;
 				break;
 			}
-				
+
 		}
 	}
-	
+
 
 	cout << now_count << endl;
 
